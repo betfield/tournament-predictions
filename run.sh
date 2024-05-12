106ae5a6eb699d72eedc0879771dd4a12b87bf03
Your Name <karpo.korotko@gmail.com>
2024-05-12 22:34:59 +0300

    sh fix

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Starting wc2022 application"
-export ROOT_URL=https://mm.fctwister.ee
-meteor --port 3033 --production --settings settings.prod.json &>> /home/betfield/logs/wc2022.log 
+echo "Starting tournament-predictions application"
+export ROOT_URL=https://em.fctwister.ee
+meteor --port 3033 --production --settings settings.prod.json &>> /home/betfield/logs/euro2024.log 

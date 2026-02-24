e3f19a8812e626084ed2d1f3f12e81a4ba271525
Karpo <karpo.korotko@gmail.com>
2026-02-24 12:49:32 +0200

    changes to startup script with log appending and allowing to gracefully start/stop service

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Starting tournament-predictions application"
 export ROOT_URL=https://nr8.ee/
-meteor --port 3033 --production --settings settings.prod.json &>> /home/betfield/logs/wc2026.log 
+exec meteor --port 3033 --production --settings settings.prod.json >> /home/betfield/logs/wc2026.log 2>&1 
